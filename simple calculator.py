de505d0a3376f759eb7362b9e8d8effdb0851eb4
reniel8to3 <renielalmariomalabo@gmail.com>
2023-05-10 15:04:11 +0800

    added operators

--- simple calculator.py
+++ simple calculator.py
@@ -1,5 +1,13 @@
 #def simple calculator main function
 #print prompt
 #define operators
+def addition:
+    return integer1 + integer2
+def subtraction:
+    return integer1 - integer2
+def multiplication:
+    return integer1 * integer2
+def division:
+    return integer1 / integer2
 #insert exceptions
 #ask user if they want to try again
