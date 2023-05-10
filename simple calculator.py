3be1a801368b72cad39860aa10ea057b8995addc
reniel8to3 <renielalmariomalabo@gmail.com>
2023-05-10 15:35:37 +0800

    more revisions

--- simple calculator.py
+++ simple calculator.py
@@ -10,7 +10,7 @@ def division(integer1, integer2):
 #def simple calculator main function and print prompt
 print ("This program is designed to function as a simple calculator. This program can perform Addition, Subtraction, Multiplication, and Division.")
 #take input from user.
-while true:
+while True:
     operation = input("What do you want the program to solve? 1 - Addition. 2 - Subtraction. 3 - Multiplication. 4- Division. ")
     if operation in ('1', '2', '3', '4'):
         try:
@@ -21,16 +21,16 @@ while true:
             continue
 
         if operation == '1':
-            print("The sum of " , integer1, "and ", integer2, "is ", addition)
+            print("The sum of " , integer1, "and ", integer2, "is ", addition(integer1, integer2))
 
         elif operation == '2':
-            print("The difference of " , integer1, "and ", integer2, "is ", subtraction)
+            print("The difference of " , integer1, "and ", integer2, "is ", subtraction(integer1, integer2))
 
         elif operation == '3':
-            print("The product of " , integer1, "and ", integer2, "is ", multiplication)
+            print("The product of " , integer1, "and ", integer2, "is ", multiplication(integer1, integer2))
 
         elif operation == '4':
-            print("The quotient of " , integer1, "and ", integer2, "is ", division)
+            print("The quotient of " , integer1, "and ", integer2, "is ", division(integer1, integer2))
 
 
 #ask user if they want to try again
